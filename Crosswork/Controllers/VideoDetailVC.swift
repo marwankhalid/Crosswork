ced88631969a137b61d5072cdaef00b330b405de
Marwan Khalid <marwankhalid44@gmail.com>
2022-02-08 17:41:17 +0500

    updated

---
+++ Crosswork/Controllers/VideoDetailVC.swift
@@ -0,0 +1,17 @@
+//
+//  VideoDetailVC.swift
+//  Crosswork
+//
+//  Created by Marwan Khalid on 2/8/22.
+//
+
+import UIKit
+
+class VideoDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+}
