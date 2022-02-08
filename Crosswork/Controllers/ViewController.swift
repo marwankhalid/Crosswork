c6b567e21d39312212d068f6a0091e08ea45301e
Marwan Khalid <marwankhalid44@gmail.com>
2022-02-08 11:56:28 +0500

    updated

--- Crosswork/Controllers/ViewController.swift
+++ Crosswork/Controllers/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchB: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -18,7 +19,24 @@ class ViewController: UIViewController {
         super.viewWillAppear(animated)
         self.navigationController?.navigationBar.isHidden = true
     }
+    
+    private func setupNavigationBar(){
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        //appearance.backgroundColor = #colorLiteral(red: 0.06475532055, green: 0.6011719108, blue: 0.553545773, alpha: 1)
+        self.navigationController?.navigationBar.standardAppearance = appearance
+        self.navigationController?.navigationBar.scrollEdgeAppearance = self.navigationController?.navigationBar.standardAppearance
+        let textAttributes = [NSAttributedString.Key.foregroundColor:UIColor.white]
+        navigationController?.navigationBar.titleTextAttributes = textAttributes
+    }
 
-
+    @IBAction func searchB(_ sender: Any) {
+        let controller = storyboard?.instantiateViewController(withIdentifier: "WebVC") as! WebVC
+        controller.modalPresentationStyle = .fullScreen
+        controller.url = URL(string: "https://crosswork.us/pre-ipo-selected-targets")
+        controller.title = "Search"
+        self.navigationController?.pushViewController(controller, animated: true)
+    }
+    
 }
 
--- Crosswork/Controllers/WebVC.swift
+++ Crosswork/Controllers/WebVC.swift
@@ -6,12 +6,26 @@
 //
 
 import UIKit
+import WebKit
 
 class WebVC: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    
+    var url:URL?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-     
+        setWebview()
     }
+    
+    private func setWebview(){
+        webView.load(URLRequest(url: url!))
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBar.isHidden = false
+    }
+    
 }
