243d6c72cf32d95ed8d0a6ccaa2416107db4accb
Marwan Khalid <marwankhalid44@gmail.com>
2022-02-17 12:44:29 +0500

    updated

--- Crosswork/Constant.swift
+++ Crosswork/Constant.swift
@@ -18,7 +18,6 @@ public class Constant {
     public static var invest = "https://www.notion.so/b8e841b85916447fad05193bafad622f#02af92fecf40466c8b25f5a44fa4d45b"
     
     public static var videoUrl = "https://crosswork.us/wp-content/uploads/2021/05/Pre-IPO-Investing_-What-happens-after-an-investment-in-a-pre-IPO-fund-CrossWork-Midas-Pre-IPO-Fund.mp4"
-    
     public static var ira = "https://app.altoira.com/register"
     public static var login = "https://portal.crosswork.us/"
     public static var bookMeeting = "https://meetings.hubspot.com/steveo1/crosswork-pre-ipo-investor"
